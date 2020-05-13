e7c57d86ba0c39e69623387e8b3b73a39f9ad087
heyujia <heyujia@aliyun.com>
2020-05-13 16:17:45 +0800

    fix numpy RunTimeError when inport mglearn

--- src/PySurprised/LinearRegression.py
+++ src/PySurprised/LinearRegression.py
@@ -1,3 +1,4 @@
+import numpy as np
 import mglearn
 
 from sklearn.linear_model import LinearRegression
--- src/PySurprised/knn_classification.py
+++ src/PySurprised/knn_classification.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import mglearn
 from sklearn.model_selection import train_test_split
